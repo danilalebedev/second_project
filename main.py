39e52f1744ed37e2a21ac08dc8c5f04c446512fa
Danila Lebedev <lebedevdanilaaa@gmail.com>
2026-04-04 11:09:48 +0300

    Work with .env

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
+# Импорт load_dotenv.
+from dotenv import load_dotenv 
+
+# Импорт библиотеки для работы с окружением.
+import os  
+
+# Загрузка переменных из .env
+load_dotenv()
 print('Банка')
 print('Улитка')
+def print_author():
+    author = os.getenv('AUTHOR')
+    print(f"Автор проекта: {author}") 
+print_author()
